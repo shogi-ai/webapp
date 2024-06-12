bd8530159e7ac8941f5996fe89b65a74f14aef94
airell <i450046@fhict.nl>
2024-06-12 15:55:38 +0200

    Added board, move, and check legal moves endpoints

--- functions/main.py
+++ functions/main.py
@@ -4,14 +4,34 @@ To get started, simply uncomment the below code or create your own.
 Deploy with `firebase deploy
 """
 
-from firebase_functions.https_fn import Request, Response, on_request
+from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
+from services.board import ShogiBoard
 
-initialize_app()
+app = initialize_app()
+shogi_board = ShogiBoard()
+    
+@https_fn.on_request(cors=options.CorsOptions(cors_origins="*", cors_methods=["get", "post"]))
+def read_board(req: https_fn.Request) -> https_fn.Response:
+    return {
+        "board": shogi_board.get_board()
+    }
 
+@https_fn.on_request()
+def make_move(req: https_fn.Request) -> https_fn.Response:
+    move_str = req.args.get("move")
+    if move_str is None:
+        return https_fn.Response("Invalid move", status=400)
+    
+    return {
+        "board": shogi_board.make_move(move_str)
+    }
 
-@on_request()
-def on_request_example(req: Request) -> Response:
-    """Test endpoint"""
-    print(req.data)
-    return Response("Hello world!")
+@https_fn.on_request()
+def read_legal_moves(req: https_fn.Request) -> https_fn.Response:
+    piece_from_square = req.args.get("from_square")
+    if piece_from_square is None:
+        return https_fn.Response("Invalid from_square", status=400)
+    return {
+        "legal_moves": shogi_board.get_legal_moves(piece_from_square)
+    }
---
+++ functions/services/board.py
@@ -0,0 +1,53 @@
+import shogi
+from shogi import Piece
+from services.board_info import SQUARES, SQUARE_NAMES, COLORS
+
+class ShogiBoard:
+    def __init__(self):
+        self.board = shogi.Board()
+    
+    def get_board(self):
+        bitboard = []
+        pieces_in_hand = []
+
+        for square in SQUARES:
+            piece = self.board.piece_at(square)
+            if piece:
+                bitboard.append(piece.symbol())
+            else:
+                bitboard.append(None)
+
+        bitboard = [bitboard[i:i + 9] for i in range(0, len(bitboard), 9)]
+
+        for color in COLORS:
+            for piece_type, piece_count in self.board.pieces_in_hand[color].items():
+                for i in range(piece_count):
+                    piece = Piece(piece_type, color)
+                    pieces_in_hand.append(piece.symbol())
+
+        return (bitboard, pieces_in_hand)
+
+    def make_move(self, move):
+        self.board.push(shogi.Move.from_usi(move))
+        return self.get_board()
+    
+    def get_legal_moves(self, from_square):
+        piece_legal_moves = []
+
+        for move in self.board.legal_moves:
+
+            from_square = move.usi()[:2]
+            to_square = move.usi()[2:]
+
+            if from_square == '2h':
+                piece_legal_moves.append(to_square)
+
+        board_builder = []
+
+        for square in SQUARE_NAMES:
+            if square in piece_legal_moves:
+                board_builder.append(square)
+            else:
+                board_builder.append(None)
+
+        return [board_builder[i:i + 9] for i in range(0, len(board_builder), 9)]
