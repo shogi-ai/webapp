6a0ca8e18313b7813614bbddcfc85ddd16036323
Lars kluijtmans <93161087+LarsKluijtmans@users.noreply.github.com>
2024-06-18 12:57:16 +0200

    Added shogi model and agent (#4)
    * Added shogi model and agent
    
    * Fix: added the model to the backend
    
    * Fix: removed old doc-strings
    
    * Fix: Updated model, to take pieces in hand into account
    
    * Fix: linting
    
    ---------
    
    Co-authored-by: airell <i450046@fhict.nl>
    Co-authored-by: LKSoftware <l.kluijtmans@student.fontys.nl>

---
+++ functions/services/ai/deep_q_network.py
@@ -0,0 +1,55 @@
+"""
+Deep Q-Network (DQN) model for processing input images and producing output
+predictions. This model includes convolutional layers, batch normalization,
+and fully connected layers, with an optional masking layer.
+"""
+
+import torch
+from torch import nn
+
+
+class DQN(nn.Module):
+    """
+    Deep Q-Network (DQN) model for processing input images and producing output
+    predictions. This model includes convolutional layers, batch normalization,
+    and fully connected layers, with an optional masking layer.
+    """
+
+    def __init__(self):
+        """
+        Initializes the DQN model with four convolutional layers, batch
+        normalization, two fully connected layers, and an optional MaskLayer.
+        """
+        super(DQN, self).__init__()
+
+        self.conv1 = nn.Conv2d(42, 16, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+
+        self.fc1 = nn.Linear(128 * 9 * 9, 128 * 81)
+        self.fc2 = nn.Linear(128 * 81, 81 * 88)
+
+    def forward(self, x, mask=None):
+        """
+        Defines the forward pass of the DQN model.
+        """
+        x = nn.functional.relu(self.bn1(self.conv1(x)))
+        x = nn.functional.relu(self.bn2(self.conv2(x)))
+        x = nn.functional.relu(self.bn3(self.conv3(x)))
+        x = nn.functional.relu(self.bn4(self.conv4(x)))
+        x = nn.Flatten()(x)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        if mask is not None:
+            x = torch.mul(x, mask)
+
+        return x
--- functions/services/game.py
+++ functions/services/game.py
@@ -4,6 +4,7 @@ from firebase_functions.https_fn import FunctionsErrorCode, HttpsError
 
 from repository.dataclasses.game import Game
 from repository.game_repository import GameRepository
+from services.ai_service import AiService
 from services.board import ShogiBoard
 
 
@@ -14,6 +15,7 @@ class GameService:
         self.app: any = app
         self.shogi_board = ShogiBoard()
         self.game_repository = GameRepository(self.app)
+        self.ai_service = AiService()
 
     def create(self) -> str:
         """Initialize new game"""
@@ -21,6 +23,33 @@ class GameService:
         game = Game(moves=[], board=bitboard, pieces_in_hand=pieces_in_hand)
         self.game_repository.create(game)
         return game.uid
+
+    def ai_move(self, uid: str):
+        """Add a new move to the game"""
+        game = self._get_game(uid)
+
+        # Have ai make a move
+        self.ai_service.setup_game(game)
+        new_move = self.ai_service.make_move()
+
+        # Update ShogiBoard instance
+        self.shogi_board.board.push(new_move)
+
+        from_square = self._index_to_square(new_move.from_square)
+        to_square = self._index_to_square(new_move.to_square)
+
+        # Update game object
+        game.moves.append(
+            {
+                "from_square": from_square,
+                "to_square": to_square,
+                "promotion": new_move.promotion,
+            }
+        )
+        game.board, game.pieces_in_hand = self.shogi_board.get_board()
+
+        # Save new game state to database
+        self.game_repository.update(game)
 
     def make_move(self, uid: str, from_square: str, to_square: str, promotion: bool):
         """Add a new move to the game"""
@@ -58,3 +87,12 @@ class GameService:
                 move["from_square"], move["to_square"], move["promotion"]
             )
         return game
+
+    @staticmethod
+    def _index_to_square(index):
+        """Get the square of the specified index"""
+        files = "987654321"  # Shogi files in reverse order
+        ranks = "abcdefghi"  # Shogi ranks
+        file = files[index % 9]  # Calculate file
+        rank = ranks[index // 9]  # Calculate rank
+        return f"{file}{rank}"
