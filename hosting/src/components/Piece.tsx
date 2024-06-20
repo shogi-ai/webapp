8f1272d9d9a18518ce42c13afb212c55dd6bb2cb
LKSoftware <l.kluijtmans@student.fontys.nl>
2024-06-20 16:06:07 +0200

    Fix: show taken peices

--- hosting/src/components/Piece.tsx
+++ hosting/src/components/Piece.tsx
@@ -3,10 +3,15 @@ import "./Piece.css";
 interface PieceProps {
   symbol: string;
   isBlack: boolean;
+  onClick?: any;
 }
 
-const Piece: React.FC<PieceProps> = ({ symbol, isBlack }) => {
-  return <div className={`piece ${isBlack ? "black" : "white"}`}>{symbol}</div>;
+const Piece: React.FC<PieceProps> = ({ symbol, isBlack, onClick }) => {
+  return (
+    <div className={`piece ${isBlack ? "black" : "white"}`} onClick={onClick}>
+      {symbol}
+    </div>
+  );
 };
 
 export default Piece;
